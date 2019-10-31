e0e3ab79644f49c3de7a37426491bb4f33df751a
Nikolas Nyby <nikolas@gnu.org>
2019-10-31 12:50:58 -0400

    Migrate to sentry-sdk

--- project_name/settings_staging.py
+++ project_name/settings_staging.py
@@ -1,6 +1,9 @@
 # flake8: noqa
 from {{project_name}}.settings_shared import *
 from ccnmtlsettings.staging import common
+from django.conf import settings
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 locals().update(
     common(
@@ -18,3 +21,11 @@ try:
     from {{project_name}}.local_settings import *
 except ImportError:
     pass
+
+# Define SENTRY_DSN in local_settings.py
+if hasattr(settings, 'SENTRY_DSN'):
+    sentry_sdk.init(
+        dsn=SENTRY_DSN,
+        integrations=[DjangoIntegration()],
+        debug=True,
+    )

15fe0e951a9db0586f0dfe263676d105343041a1
Nikolas Nyby <nikolas@gnu.org>
2019-11-08 11:43:19 -0500

    Allow flake8 to check settings files

--- project_name/settings_staging.py
+++ project_name/settings_staging.py
@@ -1,5 +1,4 @@
-# flake8: noqa
-from {{project_name}}.settings_shared import *
+from {{project_name}}.settings_shared import *  # noqa: F403
 from ccnmtlsettings.staging import common
 from django.conf import settings
 import sentry_sdk
@@ -7,25 +6,25 @@ from sentry_sdk.integrations.django import DjangoIntegration
 
 locals().update(
     common(
-        project=project,
-        base=base,
-        STATIC_ROOT=STATIC_ROOT,
-        INSTALLED_APPS=INSTALLED_APPS,
-# if you use cloudfront:
-#        cloudfront="justtheidhere",
-# if you don't use S3/cloudfront at all:
-#       s3static=False,
+        project=project,  # noqa: F405
+        base=base,  # noqa: F405
+        STATIC_ROOT=STATIC_ROOT,  # noqa: F405
+        INSTALLED_APPS=INSTALLED_APPS,  # noqa: F405
+        # if you use cloudfront:
+        #        cloudfront="justtheidhere",
+        # if you don't use S3/cloudfront at all:
+        #       s3static=False,
     ))
 
 try:
-    from {{project_name}}.local_settings import *
+    from {{project_name}}.local_settings import *  # noqa: F403
 except ImportError:
     pass
 
 # Define SENTRY_DSN in local_settings.py
 if hasattr(settings, 'SENTRY_DSN'):
     sentry_sdk.init(
-        dsn=SENTRY_DSN,
+        dsn=SENTRY_DSN,  # noqa: F405
         integrations=[DjangoIntegration()],
         debug=True,
     )
